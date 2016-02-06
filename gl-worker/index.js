5d639467d628dc70e00a0cce81df31e587a60654
Mstislav Zhivodkov <stevemyz@gmail.com>
2016-02-07 01:20:36 +0600

    Add webgl in worker

---
+++ gl-worker/index.js
@@ -0,0 +1,63 @@
+var elementButton = document.querySelector('#button');
+var elementNumber = document.querySelector('#number');
+var elementCounter = document.querySelector('#elementCounter');
+var elementResult = document.querySelector('#elementResult');
+var elementTime = document.querySelector('#elementTime');
+var elementAssert = document.querySelector('#elementAssert');
+
+var loadShaderCode = new Promise(function(resolve) {
+    var req = new XMLHttpRequest();
+    req.open('GET', '../utils/pi.glsl', true);
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            resolve(req.responseText);
+        }
+    };
+    req.send(null);
+});
+
+var canvas = document.createElement('canvas');
+var offscreen = canvas.transferControlToOffscreen();
+
+var worker = new Worker('./gpgpu.js');
+
+worker.addEventListener('message', function(ev) {
+    var res = ev.data;
+    elementTime.innerHTML = 'Time: ' + timeEnd() + 'ms';
+
+    var hx = '0123456789ABCDEF';
+    var str = '3.';
+
+    for (var i = 0; i < res.length; i++) {
+        str += hx[res[i]] + ' ';
+    }
+
+    elementResult.innerHTML = str;
+    elementCounter.innerHTML = res.length;
+    elementAssert.innerHTML = 'Equivalence: ' + assert(str);
+});
+
+elementButton.addEventListener('click', function() {
+    var value = parseInt(elementNumber.value, 10);
+    var data = new Float32Array(value);
+
+    for (var i = 0; i < data.length; i++) {
+        data[i] = i;
+    }
+
+    loadShaderCode.then(function(code) {
+        timeStart();
+
+        worker.postMessage({
+            code: code,
+            data: data,
+            canvas: offscreen,
+            options: {
+                dimension: 1,
+                fullCode: true
+            }
+        }, [
+            offscreen
+        ]);
+    });
+});
